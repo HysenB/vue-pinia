879aac2d66372c94592d29c0cc152a1e21855d78
HysenB <hysi2000@live.com>
2024-06-14 16:38:26 +0200

    storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -18,33 +18,37 @@
     </nav>
 
     <!-- loading -->
-    <div class="loading" v-if="taskStore.isLoading">Loading tasks...</div>
+    <div class="loading" v-if="isLoading">Loading tasks...</div>
 
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
 
     <!-- task list -->
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} favs left to do</p>
-      <div v-for="task in taskStore.favs">
+      <p>You have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs">
         <TaskDetails :task="task" />
       </div>
     </div>
+    <button @click="taskStore.$reset">reset</button>
   </main>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
 import TaskDetails from './components/TaskDetails.vue'
 import TaskForm from './components/TaskForm.vue'
 import { useTaskStore } from './stores/TaskStore';
 
 const taskStore = useTaskStore();
+
+const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore);
 
 taskStore.getTasks();
 
